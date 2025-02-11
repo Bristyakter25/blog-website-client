3af7a8c561ae38e8baa693c4a3383b9eb83b98b1
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-02-12 05:03:06 +0600

    added error page animation

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,18 @@
-
+import Lottie from "lottie-react";
+import errorAnimation from "../assets/lottie files/error animation.json"
+import { NavLink } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="text-3xl text-center">
-            <h2>404: Not Found Page!!!!  </h2>
+        <div className="lg:w-[850px] w-[300px] my-5 mx-auto">
+           <Lottie animationData={errorAnimation}></Lottie>
+           <NavLink to="/"><button className="btn bg-purple-300 dark:bg-purple-700 text-blue-700 dark:text-white w-full h-16 py-4 px-6 text-[20px] lg:text-2xl">
+  Back To The Homepage
+</button></NavLink>
+
         </div>
     );
 };
-
+ 
 export default ErrorPage;
 
